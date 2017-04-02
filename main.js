573e78d4b46498aa2c5bcefdfe38aaa37018afa9
sampsonli <740056710@qq.com>
2017-04-02 15:38:01 +0800

    fix bug

--- main.js
+++ main.js
@@ -4,10 +4,14 @@
 let MiniProxy = require("mini-proxy");
 
 let myProxy = new MiniProxy({
-    "port": 26352,
-    "onBeforeRequest": function(requestOptions) {
-        console.log("proxy request :" + requestOptions.host +
-            (requestOptions.path || ''));
+    port: 26352,
+    onBeforeRequest: function(requestOptions) {
+        if(~requestOptions.host.indexOf('500.com')){
+            requestOptions.host = '119.29.182.225'
+        }
+       /* console.log("proxy request :" + requestOptions.host +
+            (requestOptions.path || ''));*/
+
     }
 });
 
